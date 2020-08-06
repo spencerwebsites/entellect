a15d0fbbbe856077c2373bc9a6bb81710aa2c5d9
Morgan Spencer <morgan@spencercreative.co>
2020-08-06 08:44:54 -0400

    Update Tailwind customizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,65 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	purge: [],
+	theme: {
+		fontFamily: {
+			body: [
+				'Roboto',
+				'system-ui',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'"Segoe UI"',
+				'Roboto',
+				'"Helvetica Neue"',
+				'Arial',
+				'"Noto Sans"',
+				'sans-serif',
+				'"Apple Color Emoji"',
+				'"Segoe UI Emoji"',
+				'"Segoe UI Symbol"',
+				'"Noto Color Emoji"'
+			],
+			heading: [
+				'Comfortaa',
+				'system-ui',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'"Segoe UI"',
+				'Roboto',
+				'"Helvetica Neue"',
+				'Arial',
+				'"Noto Sans"',
+				'sans-serif',
+				'"Apple Color Emoji"',
+				'"Segoe UI Emoji"',
+				'"Segoe UI Symbol"',
+				'"Noto Color Emoji"'
+			]
+		},
+		minHeight: {
+			'screen-25': '25vh',
+			'screen-50': '50vh',
+			'screen-75': '75vh'
+		},
+		extend: {
+			colors: {
+				blue: '#182B3E',
+				purple: '#693C8D',
+				green: '#81A34B',
+				yellow: '#C6A264',
+				'purple-lt': '#DECEEE',
+				'blue-lt': '#D2DFF2',
+				'green-lt': '#D2E4A3',
+				'yellow-lt': '#F7E6C6'
+			},
+			margin: {
+				'-wrap': '-5.62vw',
+				'wrap': '5.62vw'
+			},
+			padding: {
+				'wrap': '5.62vw'
+			}
+		}
+	},
+	variants: {},
+	plugins: []
+};
